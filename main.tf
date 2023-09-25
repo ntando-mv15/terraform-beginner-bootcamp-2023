b20cc2115ab79b794e7f0df70881f370ce13595b
ntando-mv15 <88146095+ntando-mv15@users.noreply.github.com>
2023-09-25 16:41:31 +0200

    11 simple s3 bucket (#12)
    * #11 Install Terraform Provider and create S3 bucket
    
    * #11 Install Terraform Provider and create s3 bucket

--- main.tf
+++ main.tf
@@ -4,17 +4,37 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
 provider "random" {
   # Configuration options
 }
 
+#https://registry.terraform.io/providers/ContentSquare/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-    length = 16
+    lower=true
+    upper=false
+    length = 30
     special = false
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+resource "aws_s3_bucket" "example" {
+  #Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
 }
 output "random_bucket_name" {
   value = random_string.bucket_name.result
 }
+
+
+
