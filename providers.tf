db45d102a2261bc8eb7c7d8b4f9e7cee0941c702
ntando-mv15 <88146095+ntando-mv15@users.noreply.github.com>
2023-10-01 01:27:12 +0200

    #23 bucket should no longer use random provider (#24)

--- providers.tf
+++ providers.tf
@@ -11,10 +11,6 @@ terraform {
 
 terraform {
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.17.0"
--- variables.tf
+++ variables.tf
@@ -6,3 +6,16 @@ variable "user_uuid" {
     error_message = "The user_uuid value is not a valid UUID."
   }
 }
+
+
+variable "bucket_name" {
+  description = "AWS S3 bucket name"
+  type        = string
+
+  validation {
+    condition     = (
+    length(var.bucket_name) > 3 &&  length(var.bucket_name) <= 63 &&
+    can(regex("^[a-z0-9][a-z0-9]*[a-z0-9]$", var.bucket_name)))
+    error_message = "Invalid bucket name. It must be between 3 and 63 characters and can only contain lowercase letters, numbers, hyphens, and periods. It must start and end with a lowercase letter or number."
+  }
+}
